29195b22d95bdea400db6e3075752942e04b57c2
Asad <hellohello51981@gmail.com>
2025-06-11 14:43:37 +0500

    Add js - to make hamburger interactive

---
+++ JS/index.js
@@ -0,0 +1,8 @@
+
+    const hamburger = document.getElementById('hamburger-icon');
+    const navMenu = document.getElementById('nav-menu');
+
+    hamburger.addEventListener('click', () => {
+        navMenu.classList.toggle('active');
+    });
+
