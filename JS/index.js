07ab04f25326ca75b8d41bd163c1d8e07cebb851
Asad <hellohello51981@gmail.com>
2025-07-03 20:39:33 +0500

    ROOFS - ADD Js function for location changeing section

--- JS/index.js
+++ JS/index.js
@@ -15,3 +15,29 @@
       link.classList.add('active'); // add to clicked
     });
   });
+
+  const buttons = document.querySelectorAll('.filter-btn');
+  const cards = document.querySelectorAll('.card');
+
+  buttons.forEach(btn => {
+    btn.addEventListener('click', () => {
+      // Set active class
+      buttons.forEach(b => b.classList.remove('active'));
+      btn.classList.add('active');
+
+      const type = btn.getAttribute('data-type');
+
+      cards.forEach(card => {
+        const cardType = card.getAttribute('data-type');
+        if (type === 'all') {
+          card.classList.remove('hidden');
+        } else {
+          if (cardType === type) {
+            card.classList.remove('hidden');
+          } else {
+            card.classList.add('hidden');
+          }
+        }
+      });
+    });
+  });
